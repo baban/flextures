32b6c738c2c3465fe6949bb58616ed4ad070c688
baban <babanba.n@gmail.com>
2011-11-24 23:45:24 +0900

    テストを削除（使っていない）履歴を更新

--- test/test_flextures.rb
+++
@@ -1,7 +0,0 @@
-require 'helper'
-
-class TestFlextures < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
