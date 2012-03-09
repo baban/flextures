4dfb50e6cd74ed8724b3c0eb3e3c1ff87a08904c
baban <babanba.n@gmail.com>
2012-03-10 08:41:59 +0900

    1.9.6系

--- flextures.gemspec
+++ flextures.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{flextures}
-  s.version = "1.9.4"
+  s.version = "1.9.5"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["baban"]
-  s.date = %q{2012-03-05}
+  s.date = %q{2012-03-10}
   s.description = %q{load and dump fixtures}
   s.email = %q{babanba.n@gmail.com}
   s.extra_rdoc_files = [
--- lib/flextures/flextures_base_config.rb
+++ lib/flextures/flextures_base_config.rb
@@ -3,17 +3,17 @@
 # 基本設定を記述する
 module Flextures
   module Config
-    READ_ONLYS=[]
+    @@read_onlys=[]
     @@configs={
       fixture_load_directory: "spec/fixtures/",
       fixture_dump_directory: "spec/fixtures/",
-      init_all_tables: true,  # 実行後に全テーブルの初期化を行うか？falseにするとそのぶん高速化できる
+      init_all_tables: false,  # 実行後に全テーブルの初期化を行うか？falseにするとそのぶん高速化できる
     }
     # ハッシュをsetter、getterに変換
     class<< self
       @@configs.each do |setting_key, setting_value|
-        define_method(setting_key){ @@configs[setting_key] } unless READ_ONLYS.include?(setting_key)
-        define_method("#{setting_key}="){ |arg| @@configs[setting_key]=arg }
+        define_method(setting_key){ @@configs[setting_key] }
+        define_method("#{setting_key}="){ |arg| @@configs[setting_key]=arg } unless @@read_onlys.include?(setting_key)
       end
     end
   end
