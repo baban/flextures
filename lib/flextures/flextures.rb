87058995498f23729f91c0d470e9de3b2a1d9522
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-03-19 17:19:22 +0900

    nullの値をダンプするときの警告

--- lib/flextures/flextures.rb
+++ lib/flextures/flextures.rb
@@ -95,6 +95,20 @@ module Flextures
   module Dumper
     PARENT = Flextures
 
+    TRANSLATER = {
+      binary:->(d){ d.to_i },
+      boolean:->(d){ (0==d || ""==d || !d) ? false : true },
+      date:->(d){ Date.parse(d.to_s) },
+      datetime:->(d){ DateTime.parse(d.to_s) },
+      decimal:->(d){ d.to_i },
+      float:->(d){ d.to_f },
+      integer:->(d){ d.to_i },
+      string:->(d){ d.to_s },
+      text:->(d){ d.to_s },
+      time:->(d){ DateTime.parse(d.to_s) },
+      timestamp:->(d){ DateTime.parse(d.to_s) },
+    }
+
     # 適切な型に変換
     def self.trans v
       case v
@@ -128,14 +142,22 @@ module Flextures
       table_name = format[:table]
       klass = PARENT::create_model(table_name)
       attributes = klass.columns.map { |colum| colum.name }
+
+      columns = klass.columns
+      # テーブルからカラム情報を取り出し
+      column_hash = {}
+      columns.each { |col| column_hash[col.name] = col }
+      # 自動補完が必要なはずのカラム
+      lack_columns = columns.select { |c| !c.null and !c.default }.map{ |o| o.name.to_sym }
+      not_nullable_columns = columns.select { |c| !c.null }.map &:name
+
       File.open(outfile,"w") do |f|
         klass.all.each_with_index do |row,idx|
           f<< "#{table_name}_#{idx}:\n" +
-            attributes.map { |column|
-              v = trans row.send(column)
-              v.kind_of?(String) ?
-                "  #{column}: |-\n    #{v.gsub(/\n/,%Q{\n    })}\n":  # Stringだと改行が入るので特殊処理
-                "  #{column}: #{v}\n"
+            attributes.map { |col|
+              v = trans row.send(col)
+              v = "|-\n    " + v.gsub(/\n/,%Q{\n    }) if v.kind_of?(String) # Stringだと改行が入るので特殊処理
+              "  #{column}: #{v}\n"
             }.join
         end
       end
