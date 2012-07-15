b8e6b5dde48ecf5f848a4b86ebd5db97069ce73f
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-07-15 21:30:41 +0900

    dump filter組み込みテスト

--- lib/flextures/flextures_factory.rb
+++ lib/flextures/flextures_factory.rb
@@ -30,7 +30,30 @@ module Flextures
     def self.[](table_name); self.get(table_name); end
   end
   class DumpFilter
-    
+    # 設置ファイルから取得した Factoryの一覧を取得
+    FACTORIES={}
+
+    # Factory を定義
+    # @params table_name
+    # @params options
+    # @params block
+    # @return Flextures::Factory
+    def self.define table_name, *options, &block
+      h={ block: block }
+      options.each do |o|
+        begin
+          h[:model] = o if o.new.is_a? ActiveRecord::Base
+        rescue
+        end
+      end
+      FACTORIES[table_name.to_sym]=h
+    end
+
+    # Factoryを取得
+    def self.get table_name, key
+      f = FACTORIES[table_name.to_sym]
+      f && f[:block]
+    end
   end
   Factory = LoadFilter
 end
