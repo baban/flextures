8aae8fe7414e7b335bb8196dc45e52d5af41426e
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-06-08 10:22:19 +0900

    マニュアルの整備とコメントの英語化

--- lib/flextures/flextures_base_config.rb
+++ lib/flextures/flextures_base_config.rb
@@ -1,16 +1,25 @@
 # encoding: utf-8
 
-# 基本設定を記述する
+# base configurations
+# if you want to change setting, create 'config/flextures.config.rb', and overwrite setting
+#
+# example:
+# module Flextures
+#  # load and dump directroy setting change "spec/fixtures/" to "test/fixtures/"
+#  Config.fixture_load_directory = "test/fixtures/"
+#  Config.fixture_dump_directory = "test/fixtures/"
+# end
+#
 module Flextures
   module Config
     @@read_onlys=[]
     @@configs={
-      ignore_tables: ["schema_migrations"],
-      fixture_load_directory: "spec/fixtures/",
-      fixture_dump_directory: "spec/fixtures/",
-      init_all_tables: false,  # 実行後に全テーブルの初期化を行うか？falseにするとその分高速化できる
-      options: [],
-      table_load_order: [],
+      ignore_tables: ["schema_migrations"], # 'ignore_tables' table data is not deleted by flextures delete_all method
+      fixture_load_directory: "spec/fixtures/", # base load directory
+      fixture_dump_directory: "spec/fixtures/", # dump load directory
+      init_all_tables: false,  # if this option is 'true', when start unit test, all table data is delete
+      options: {}, # options(example { unfilter: true })
+      table_load_order: [], # set load options
     }
     # hash key change to getter and setter
     class<< self
